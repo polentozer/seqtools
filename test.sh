4f376cbcc038870a53a2d013f632f85422aa1e86
polentozer <markus.tadej@gmail.com>
2018-04-06 00:35:14 +0200

    simple testing script

--- test.sh
+++ test.sh
@@ -1,7 +1,24 @@
-#!/bin/sh
-echo "testing"
+#!/bin/bash
+echo -e "\nTesting...\n"
 mkdir ./temp_test
-seqtools -p -t test_data/table.csv -f test_data/dna.fasta -s temp_test/temp_dna.fasta
-seqtools -p -t test_data/table.csv -f test_data/dna.fasta -s temp_test/temp_dna.fasta
-seqtools -t test_data/table.csv -f test_data/dna.fasta -s temp_test/temp_dna.fasta
-seqtools -t test_data/table.csv -f test_data/multi_dna.fasta -s temp_test/temp_multi_dna.fasta
+touch ./log
+test=0
+python3 seqtools -t test_data/table.csv -i test_data/prot.fasta -o temp_test/prot.fasta -pf
+python3 seqtools -t test_data/table.csv -i test_data/multi_prot.fasta -o temp_test/multi_prot.fasta -pf
+python3 seqtools -t test_data/table.csv -i test_data/dna.fasta -o temp_test/dna.fasta -f
+python3 seqtools -t test_data/table.csv -i test_data/multi_dna.fasta -o temp_test/multi_dna.fasta -f
+for file in ./temp_test/*
+do
+    if cmp -s "${file}" "./test_data/solution_${file:12}"; then 
+        echo -e "${file:12}: OK" >> ./log
+    else
+        test=1
+        echo -e "${file:12}: BAD" >> ./log
+    fi
+done
+if [ $test -eq 0 ]; then
+    echo -e "\nAll's well."
+    rm -r temp_test log
+else
+    echo -e "\nOops, there were some problems."
+fi
