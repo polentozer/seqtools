47dbd70080fde96f01a2f5120cd2544000878f14
polentozer <markus.tadej@gmail.com>
2018-04-09 12:38:07 +0200

    relative paths work now

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ echo -e "\nTesting...\n"
 mkdir ./temp_test
 touch ./log
 test=0
-python3 seqtools -t test_data/table.csv -i test_data/prot.fasta -o temp_test/prot.fasta -pf
-python3 seqtools -t test_data/table.csv -i test_data/multi_prot.fasta -o temp_test/multi_prot.fasta -pf
-python3 seqtools -t test_data/table.csv -i test_data/dna.fasta -o temp_test/dna.fasta -f
-python3 seqtools -t test_data/table.csv -i test_data/multi_dna.fasta -o temp_test/multi_dna.fasta -f
+seqtools -t test_data/table.csv -i test_data/prot.fasta -o temp_test/prot.fasta -pf
+seqtools -t test_data/table.csv -i test_data/multi_prot.fasta -o temp_test/multi_prot.fasta -pf
+seqtools -t test_data/table.csv -i test_data/dna.fasta -o temp_test/dna.fasta -f
+seqtools -t test_data/table.csv -i test_data/multi_dna.fasta -o temp_test/multi_dna.fasta -f
 for file in ./temp_test/*
 do
     if cmp -s "${file}" "./test_data/solution_${file:12}"; then 
