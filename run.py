74aef07f280d032f0117547c70718420b1897e76
polentozer <markus.tadej@gmail.com>
2019-03-14 15:37:39 +0100

    addition to last commit

---
+++ run.py
@@ -0,0 +1,5 @@
+from seqtools import main
+
+
+if __name__ == '__main__':
+    main()
--- test/test.py
+++ test/test.py
@@ -1,25 +1,30 @@
-import seqtools
-from os import path
-x = (None, False, True)
+# import seqtools
+# from os import path
+# x = (None, False, True)
 
-print(x)
-print(len(x))
+# print(x)
+# print(len(x))
 
-if x[0]:
-    print(f"x[0]: {x[0]}")
+# if x[0]:
+#     print(f"x[0]: {x[0]}")
 
-if x[1]:
-    print(f"x[1]: {x[1]}")
+# if x[1]:
+#     print(f"x[1]: {x[1]}")
 
-if x[2]:
-    print(f"x[2]: {x[2]}")
+# if x[2]:
+#     print(f"x[2]: {x[2]}")
 
-bla = [0,0,1,1]
+# bla = [0,0,1,1]
 
-print(len(bla), sum(bla))
+# print(len(bla), sum(bla))
 
-test = "axbabdfadfbdfagfdavdsavsdav"
-test1 = [0,1,2,3,4,5,6,7,8,9,10]
+# test = "axbabdfadfbdfagfdavdsavsdav"
+# test1 = [0,1,2,3,4,5,6,7,8,9,10]
 
-for x,y in zip(test, test1):
-    print(x,y)
+# for x,y in zip(test, test1):
+#     print(x,y)
+
+
+from seqtools.generate import generate_dna
+
+generate_dna(30)
